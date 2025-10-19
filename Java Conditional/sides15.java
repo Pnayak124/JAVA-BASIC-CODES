6df7e5c81842ad5deb3525a77161464b2599dd42
PAWAN KUMAR NAYAK <150213925+Pnayak124@users.noreply.github.com>
2025-10-19 22:24:12 +0530

    Update sides15.java

--- Java Conditional/sides15.java
+++ Java Conditional/sides15.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class sides15 {
     public static void main(String[] args) {
-           Scanner sc = new Scanner(System.in);
-        int side1 = sc.nextInt();
+    Scanner sc = new Scanner(System.in);
+    int side1 = sc.nextInt();
 	int side2 = sc.nextInt();
 	int side3 = sc.nextInt();
         if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
